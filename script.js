fed8e8a616a610f8ee94735e387a4d9eeb6531f1
DARAMOLA DANIEL <dandaramola.d@gmail.com>
2022-08-21 21:10:48 +0100

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const navbar = document.querySelector('.nav_bar');
+
+// This is for add effect to the navbar once the user scrolls
+
+const navbar = document.querySelector('.l_header');
 window.onscroll = () => {
     if (window.scrollY > 50) {
         navbar.classList.add('nav-active');
@@ -6,3 +9,32 @@ window.onscroll = () => {
         navbar.classList.remove('nav-active');
     }
 };
+
+// This is to make the Hembuerge and close functional 
+
+
+const navMenu = document.getElementById("nav_menu"),
+     navToggle = document.getElementById("nav_toggle"),
+     navClose = document.getElementById("nav_close");
+
+
+/*=============== SHOW MENU ===============*/
+/* validate if constant exists  */
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-menu")
+    })
+}
+if(navClose) {
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-menu")
+    })
+}
+
+const navLink = document.querySelectorAll(".nav_links");
+
+function linkAction() {
+    navMenu.classList.remove("show-menu")
+}
+
+navLink.forEach(n => n.addEventListener("click", linkAction))
